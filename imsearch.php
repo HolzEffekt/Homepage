b97b73fe345d3ef461bc41f0f2b7387f63b6929e
HolzEffekt <d.spliesgar@gmail.com>
2026-05-18 14:27:03 +0200

    Add files via upload

--- imsearch.php
+++ imsearch.php
@@ -2,11 +2,15 @@
 <!DOCTYPE html><!-- HTML5 -->
 <html prefix="og: http://ogp.me/ns#" lang="de-DE" dir="ltr">
 	<head>
-		<title>Suchen - Neues Projekt</title>
+		<title>Suchen - HolzEffekt</title>
 		<meta charset="utf-8" />
 		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
-		<meta name="author" content="Incomedia" />
+		<meta name="author" content="Dennis Spliesgar" />
 		<meta name="generator" content="Incomedia WebSite X5 Evo 2023.2.6 - www.websitex5.com" />
+		<meta property="og:image" content="http://localhost/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="1179">
+		<meta property="og:image:height" content="526">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
 		<link rel="stylesheet" href="style/reset.css?2023-2-6-0" media="screen,print" />
@@ -15,7 +19,7 @@
 		<link rel="stylesheet" href="style/template.css?2023-2-6-0" media="screen" />
 		
 		<link rel="stylesheet" href="pluginAppObj/imFooter_pluginAppObj_03/custom.css" media="screen, print" />
-		<link rel="stylesheet" href="pcss/imsearch.css?2023-2-6-0-639147100114863053" media="screen,print" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2023-2-6-0-639147111064264110" media="screen,print" />
 		<script src="res/jquery.js?2023-2-6-0"></script>
 		<script src="res/x5engine.js?2023-2-6-0" data-files-version="2023-2-6-0"></script>
 		
@@ -25,7 +29,7 @@
 			x5engine.utils.currentPagePath = 'imsearch.php';
 			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
 		</script>
-		<link rel="icon" href="favicon.png?2023-2-6-0-639147100114863053" type="image/png" />
+		<link rel="icon" href="favicon.png?2023-2-6-0-639147111064107845" type="image/png" />
 	</head>
 	<body>
 		<div id="imPageExtContainer">
@@ -34,7 +38,7 @@
 				<div id="imFooterBg"></div>
 				<div id="imPage">
 					<header id="imHeader">
-						<h1 class="imHidden">Suchen - Neues Projekt</h1>
+						<h1 class="imHidden">Suchen - HolzEffekt</h1>
 						<div id="imHeaderObjects"></div>
 					</header>
 					<div id="imStickyBarContainer">
